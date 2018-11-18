a337553e176d40c96db71aece1cb86791e40825c
Aleksandr Lisianoi <alisianoi@protonmail.com>
2018-11-18 16:53:28 +0100

    Update template to use i32/i64/u32/u64

--- template/a.cpp
+++ template/a.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
